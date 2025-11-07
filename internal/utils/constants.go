1173d0dfbd7c6bf0a0a05021512e1438b6773b44
ssuf1998dev <ssuf1998dev@126.com>
2025-11-07 06:32:55 +0000

    feat: version

--- internal/utils/constants.go
+++ internal/utils/constants.go
@@ -1,13 +1,18 @@
 package utils
 
 import (
+	_ "embed"
 	"fmt"
+	"strings"
 
 	"github.com/Masterminds/semver/v3"
 )
 
+//go:embed version.txt
+var version string
+
 var Crac = "crac"
 var CreatedByCracMeta = "CRACMETA"
 var CreatedByCracCopy = "CRACCOPY"
-var CracVersion = semver.MustParse("1.0.0")
+var CracVersion = semver.MustParse(strings.TrimSpace(version))
 var CracVersionConstraint, _ = semver.NewConstraint(fmt.Sprintf(">= %d < %d", CracVersion.Major(), CracVersion.Major()+1))
